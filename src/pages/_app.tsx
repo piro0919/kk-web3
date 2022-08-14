0d572d681cb949374181d32f206a0c93d34ffd3f
piro <piro.haniwa@gmail.com>
2022-08-15 06:58:00 +0900

    feat: support google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Layout from "components/Layout";
 import ScrollToTop from "components/ScrollToTop";
 import "github-markdown-css";
 import type { AppProps } from "next/app";
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 import NextNProgress from "nextjs-progressbar";
 import { Toaster } from "react-hot-toast";
 import "ress";
@@ -13,8 +14,11 @@ import "styles/mq-settings.scss";
 import "styles/szh-menu.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  usePageViews();
+
   return (
     <>
+      <GoogleAnalytics />
       <Layout>
         <Component {...pageProps} />
       </Layout>
