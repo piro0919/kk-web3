d5eb1cb53d9302865976a8db6006b475bb516c0c
piro <piro.haniwa@gmail.com>
2022-08-15 11:12:46 +0900

    ci: enable analyze

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const path = require("path");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withBundleAnalyzer({
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -29,6 +32,6 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "src/styles")],
   },
   swcMinify: true,
-};
+});
 
 module.exports = nextConfig;
